7955296e18a0d7ae5d74c5ad5f1e0eef20e1af56
Mr-man7352 <manissh7352@gmail.com>
2021-04-29 20:49:19 +0530

    added like button

--- src/components/chat-window/messages/MessageItem.js
+++ src/components/chat-window/messages/MessageItem.js
@@ -5,6 +5,7 @@ import { useCurrentRoom } from '../../../context/current-room.context';
 import { auth } from '../../../misc/firebase';
 import ProfileAvatar from '../../dashboard/ProfileAvatar';
 import PresenceDot from '../../PresenceDot';
+import IconBtnControl from './IconBtnControl';
 import ProfileInfoModal from './ProfileInfoModal';
 
 const MessageItem = ({ message, handleAdmin }) => {
@@ -44,6 +45,14 @@ const MessageItem = ({ message, handleAdmin }) => {
         <TimeAgo
           datetime={createdAt}
           className="font-normal text-black-45 ml-2"
+        />
+        <IconBtnControl
+          {...(true ? { color: 'red' } : {})}
+          isVisible
+          iconName="heart"
+          tooltip="Like this message "
+          onClick={() => {}}
+          badgeContent={5}
         />
       </div>
       <div>
